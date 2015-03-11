e9f6d8e996db3959f5393c51fe4e4d369cc2b539
Mark Kellogg <mkkellogg@gmail.com>
2015-03-10 23:02:32 -0700

    Added forward camera vector member variable.

--- src/gtedemo/scene.cpp
+++ src/gtedemo/scene.cpp
@@ -16,7 +16,7 @@ class LavaField;
 
 Scene::Scene()
 {
-
+	baseCameraForward = Vector3::Forward;
 }
 
 Scene::~Scene()
--- src/gtedemo/scene.h
+++ src/gtedemo/scene.h
@@ -1,3 +1,12 @@
+/*
+ * Class: Scene
+ *
+ * Author: Mark Kellogg
+ *
+ * Part of the GTE engine demo, Scene is a base class
+ * for grouping scene objects and components.
+ */
+
 #ifndef _GTE_SCENE_H_
 #define _GTE_SCENE_H_
 
@@ -17,14 +26,19 @@ class LavaField;
 
 class Scene
 {
+	protected:
+
+	Vector3 baseCameraForward;
+
 	public:
 
 	Scene();
 	virtual ~Scene();
 
 	virtual SceneObjectRef GetSceneRoot() = 0;
+	virtual void OnActivate() = 0;
 	virtual void Update() = 0;
-	virtual void Setup(AssetImporter& importer, SceneObjectRef ambientLightObject, SceneObjectRef directLightObject, SceneObjectRef playerObject) = 0;
+	virtual void Setup(AssetImporter& importer, SceneObjectRef ambientLightObject, SceneObjectRef directionalLightObject, SceneObjectRef playerObject) = 0;
 };
 
 #endif
